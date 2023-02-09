8609cdcd5a48efdd45f6b165db887c3bffe9ffa6
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-09 23:43:34 +0530

    practice

---
+++ details.c
@@ -0,0 +1,15 @@
+//wap to print detail of a student like name , reg no, roll no, city, state, contact no. and cgpa
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Name : Atul Kumar Tiwari\n");
+	printf("Reg. no. : 12221533\n");
+	printf("Roll no. : 71\n");
+	printf("city : Varanasi\n");
+	printf("State : Uttar Pradesh\n");
+	printf("contact no. : 9336980842\n");
+	printf("cgpa: 0");
+	return 0;
+}
