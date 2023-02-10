6087f7bf1a192c141126759c601b38c1553416fc
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-10 10:50:45 +0530

    operators

---
+++ unary_opr2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter value of x: ");
+    scanf("%d",&x);
+    y=x++;
+    z=++x;
+    printf("\n%d,%d,%d",x,y,z);
+    return 0;
+}
