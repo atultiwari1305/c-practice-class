008d5c6b00371e39487ef2af05d554e98a83e98f
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-13 21:55:03 +0530

    new program

---
+++ speed.c
@@ -0,0 +1,12 @@
+//wap to print speed
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float dist=11;
+	float time=2;
+	float speed=dist/time;
+	printf("Speed is : %f",speed);
+	return 0;
+}
