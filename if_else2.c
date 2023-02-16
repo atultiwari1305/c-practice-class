9a6a1681d54184e8a9e939d7bdd567cb44c115d0
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-16 12:51:58 +0530

    else and if

---
+++ if_else2.c
@@ -0,0 +1,22 @@
+//wap to find a number is less or greater than other
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter your number: ");
+    scanf("%d",&a);
+    printf("Enter the value with which you want compare: ");
+    scanf("%d",&b);
+
+    if(a<b)
+    {
+        printf("%d is less than %d",a,b);
+    }
+    else
+    {
+        printf("%d is less than %d",a,b);
+    }
+    return 0;
+
+}
