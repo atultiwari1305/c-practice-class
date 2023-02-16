2f917f530baf7c98968a85a7a036a42c1229d608
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-16 11:43:14 +0530

    relational operator

---
+++ relational_opr.c
@@ -0,0 +1,14 @@
+//wap to understand relational operators
+
+#include<stdio.h>
+int main()
+
+{
+    int a=1,b=2,c,d;
+    c=a>b;
+    printf("\n%d",c);
+    
+    d = a!=b;
+    printf("\n%d",d);
+    return 0;
+}
