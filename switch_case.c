1044bab7b9c290bd6d4cbb9f08c42cdc7cf156ac
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-16 12:49:43 +0530

    switch cases

---
+++ switch_case.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int pt;
+    printf("Enter the number of nodes: ");
+    scanf("%d",&pt);
+    switch(pt)
+    {
+        case 0:
+        printf("\nNo Geometry");
+        break;
+
+        case 1:
+        printf("\nA point");
+        break;
+
+        case 2:
+        printf("\nA line");
+        break;
+
+        case 3:
+        printf("\nA triangle");
+        break;
+
+        case 4:
+        printf("\nA retangle");
+        break;
+
+        case 5:
+        printf("\nA pentagon");
+        break;
+    }
+}
