a513189610d9996d20d3b42a9094568291aba968
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-18 11:56:43 +0530

    multiplication table

---
+++ multiplication_table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter number for multiplication table: ");
+    scanf("%d",&n);
+    while(i<=10)
+    {
+        printf("\n%d X %d = %d",n,i,n*i);
+        i++;
+    }
+    return 0;
+}
