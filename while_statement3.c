3605ad34626ca38b77bc554f92d17bcac909f19b
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-18 11:56:14 +0530

    while loop

---
+++ while_statement3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n=1,a;
+    printf("Enter the value of n upto which it should print: ");
+    scanf("%d",&a);
+    while(n<=a)
+    {
+        printf("\n%d",n);
+        n=n+2;
+    }
+    return 0;
+}
