415ed43375e1f0132f5a9f5adaca9774b67e4ccc
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-18 11:34:28 +0530

    while loop

---
+++ while_statement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        printf("\n%d",n);
+        n--;
+    }
+    return 0;
+}
