2915a9140d91e68bed18a16fa99215102fa5498f
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-02-28 10:10:15 +0530

    getchar and putchar

---
+++ getchar_putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the character you want to print: ");
+    ch=getchar();
+    printf("Your character is: ");
+    putchar(ch);
+    return 0;
+}
