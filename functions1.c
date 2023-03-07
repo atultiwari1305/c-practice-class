eb73926ba8e3836444901704d98e37b5d31508e2
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-03-07 09:46:46 +0530

    functions

---
+++ functions1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int square(int y);
+
+int main()
+{
+    int x;
+    for(x=1;x<=10;++x)
+    {
+        printf("%d",square(x));
+        puts(" ");
+    }
+    return 0;
+}
+
+int square(int y)
+{
+    return y*y;
+}
