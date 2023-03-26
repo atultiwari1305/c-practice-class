2436f32d317d1d30c65af0b98394fa8b8f10cb39
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-03-26 23:52:14 +0530

    array

---
+++ array_2D_dyn.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("Enter the value of a[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Elements of 2D matrix are: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\t%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
